c75d61da5b2c00bfe67509b6fb0c0440d3bc1c6e
«SARO» <opac4321@mail.ru>
2023-01-15 14:49:37 +0300

    end programs

--- dz/Program.cs
+++ dz/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+
+void SortArray(string[] array)
+
+{
+    
+    int countLength = 3;
+    
+    for (int i = 0; i <array.GetLength(0); i++)
+    {
+        if (array[i].Length > countLength)
+        {
+          array[i] = null;
+        }
+        
+
+    }
+    
+}
+void PrintArray(string[] arr)
+{
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+// System.Console.Write($"[ {arr[i]} ] ");
+System.Console.Write('[' + (arr[i]) + ']');
+}
+System.Console.WriteLine();
+}
+
+System.Console.WriteLine(" array:");
+string[] array = { "Zenit", "hello world", "5", "no", "I" };
+System.Console.WriteLine('[' + string.Join(",", array) + ']');
+SortArray(array);
+PrintArray(array);
